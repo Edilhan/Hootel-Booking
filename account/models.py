cf4d2a90ef88fa661f07eb61f4ba7245c0d7d3ce
myrzaiym00 <myrzaiym.00.01@gmail.com>
2022-08-16 12:54:56 +0600

    account_update

--- account/models.py
+++ account/models.py
@@ -1,39 +1,33 @@
 
-from django.contrib.auth.base_user import BaseUserManager
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db import models
-
-
-GenderChoises = [
-    ('M' ,'male'),
-    ('F' ,'female')]
 
 
 class UserManager(BaseUserManager):
     use_in_migrations = True
 
-    def create_user(self, email, password, **extra_fields):
+    def create_user(self, email,  first_name, last_name, password):
         if not email:
             raise ValueError("Email is required")
+        if not first_name:
+            raise ValueError("First name is required")
+        if not last_name:
+            raise ValueError("Last name is required")
         
         email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
+        user = self.model(email=email, first_name=first_name, last_name=last_name)
         user.set_password(password)
-        user.is_active = False
         user.send_activation_code()
         user.save(using=self._db)
         return user
     
     
-    def create_superuser(self, email, password, **extra_fields):
-        if not email:
-            raise ValueError("Email is required")
-        
+    def create_superuser(self, email, first_name, last_name, password):
         email = self.normalize_email(email)
-        user = self.model(email=email, last_name='ab', gender='M' **extra_fields)
+        user = self.model(email=email, first_name=first_name, last_name=last_name)
         user.set_password(password)
-
         user.is_active = True
+        user.is_admin = True
         user.is_superuser = True
         user.is_staff = True
 
@@ -42,18 +36,34 @@ class UserManager(BaseUserManager):
 
 
 
-class User(AbstractUser):
-    email = models.EmailField(max_length=150, unique=True)
-    first_name = models.CharField(max_length=150)
-    last_name = models.CharField(max_length=30)
+class User(AbstractBaseUser):
+    email = models.EmailField(max_length=100, unique=True)
+    username = models.CharField(max_length=30, unique=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
     activation_code = models.CharField(max_length=8, blank=True)
-    phone_number = models.IntegerField()
-    gender = models.CharField(max_length=1,choices=GenderChoises)
+    phone_number = models.CharField(max_length=13)
+   
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ["first_name", "last_name"]
 
     objects = UserManager()
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+
+    def has_module_perms(self, app_label):
+        return True
 
     def generate_activation_code(self):
         from django.utils.crypto import get_random_string
